9cd067bd682f95dc36c2010da3546f0a8877eb74
Vadim Semenov <bagum.cemehob@gmail.com>
2024-05-04 21:29:41 +0300

    change instrument view layout

--- src/views/Instrument.vue
+++ src/views/Instrument.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="row inline-box">
-    <div class="row title">{{ instrument.name }}</div>
-    <div v-if="instrumentService">
-      <div class="row">Current party:{{ currentParty.name }}</div>
-      <div class="row">Upcoming party: {{ upcomingParty.name }} {{ prerollCountDown }}
+  <div class="inline-box w-50">
+    <div class="title">{{ instrument.name }}</div>
+    <div v-if="instrumentService" class="party-info w-100 h-100">
+      <div class="current-party">
+        <div class="d-flex" v-if="currentParty.name">
+          <div class="fw-bold"> {{ currentParty.name }}</div>
+          <div v-if="countDown.current" class="count-down"> {{ countDown.current}} </div>
+          <div v-if="countDown.common" class="count-down red"> {{ countDown.common}} </div>
+        </div>
+        <div class="d-flex" v-else>
+          <div>-</div>
+          <div v-if="countDown.upcoming" class="count-down blue"> {{ countDown.upcoming}} </div>
+        </div>
+      </div>
+      <div v-if="upcomingParty.name" class="upcoming-party">
+         Next: <span class="fw-bold">{{ upcomingParty.name }}</span>
       </div>
     </div>
   </div>
@@ -31,8 +42,8 @@ export default {
     upcomingParty() {
       return this.instrumentService.upcomingParty(this.currentBeat)
     },
-    prerollCountDown() {
-      return this.instrumentService.prerollCountDown(this.currentBeat)
+    countDown() {
+      return this.instrumentService.countDown(this.currentBeat)
     }
   },
   mounted() {
@@ -51,4 +62,36 @@ export default {
 .title {
   border-bottom: 1px solid #7d858d;
 }
+
+.current-party {
+  font-size: 20px;
+}
+
+.current-party .d-flex {
+  justify-content: space-between;
+  align-items: center;
+}
+.current-party div {
+  margin: 10px;
+}
+.count-down {
+  font-size: 30px;
+}
+
+.count-down.red {
+  color: red;
+}
+.count-down.blue {
+  color: blue;
+}
+.party-info {
+ position: relative;
+}
+.upcoming-party {
+  border: 1px solid #7d858d;
+  position: absolute;
+  right: 0;
+  top: -11px;
+}
+
 </style>
