271d47103d87c8f41a1f5d47840a6fa3f987c541
Vadim Semenov <bagum.cemehob@gmail.com>
2024-05-09 18:46:25 +0300

    add track info to navigation bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,21 @@
 <template>
- <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
- <div class="container-fluid ">
-
-<router-link to="/" class="navbar-brand">
-  <SvgIcon :name="'logo'"></SvgIcon> Kapellmeister
-</router-link>
-
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid ">
+      <router-link to="/" class="navbar-brand">
+        <SvgIcon :name="'logo'"></SvgIcon>
+        Kapellmeister
+      </router-link>
+      <div class="track-name">
+        <div>{{ pattern.name }}</div>
+        <div v-if="pattern">
+          <span class="track-info">{{ pattern.tempo }} bpm</span>
+          <span class="track-info">{{ pattern.duration }} measures</span>
+          <span class="track-info">{{ durationInBeats }} beats</span>
+          <span class="track-info">{{ duration }}</span>
+        </div>
+      </div>
     </div>
- </nav>
+  </nav>
 </template>
 
 <script setup>
@@ -15,13 +23,40 @@ import SvgIcon from "@/components/SvgIcon.vue";
 </script>
 
 <script>
-// @ is an alias to /src
 
+import {ConductorService} from "@/services/ConductorService";
 
 export default {
   name: 'NavBar',
-  components: {
-
+  components: {},
+  computed: {
+    pattern() {
+      return this.$store.state.pattern
+    },
+    durationInBeats() {
+      if (!this.pattern.measure) return 0
+      return this.pattern.duration * this.pattern.measure.beats
+    },
+    duration() {
+      return ConductorService.calculateDuration(this.durationInBeats, this.pattern.tempo)
+    }
   }
 }
 </script>
+<style scoped>
+.track-name {
+  color: #fff
+}
+
+.track-info {
+  color: #999;
+  padding: 0 10px;
+  border-left: 1px solid #777;
+}
+
+.track-info:first-child {
+  padding: 0;
+  border: 0;
+  padding-right: 10px;
+}
+</style>
