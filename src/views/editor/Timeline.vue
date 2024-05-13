77fe320a25a3f69a8b858d1d71ac7a672a0805db
Vadim Semenov <bagum.cemehob@gmail.com>
2024-05-13 20:13:52 +0300

    add methods to instrument

--- src/views/editor/Timeline.vue
+++ src/views/editor/Timeline.vue
@@ -4,7 +4,7 @@
       <tr>
         <th class="narrow-column"> measure</th>
         <th v-for="(instrument, i) in pattern.instruments" v-bind:key="i"
-            @contextmenu="e => showContextMenu(e, instrumentThId(instrument, i))"
+            @contextmenu="e => showContextMenu(e, instrument, i)"
             :id="instrumentThId(instrument, i)">
           <InstrumentNameField :instrument @name="n => instrument.name = n" />
         </th>
@@ -21,9 +21,13 @@
       </tr>
     </table>
     <div v-show="contextMenuShown" class="context-menu" :style="getContextMenuStyle()">
+      <div class="menu-title">Instrument:</div>
       <ul>
-        <li>delete instrument</li>
-        <li>add party</li>
+        <li @click="deleteInstrument()"><i class="fa fa-trash"></i> delete</li>
+        <li @click="addInstrumentRight()"><i class="fa fa-plus"></i> add after</li>
+        <li @click="addInstrumentLeft()"><i class="fa fa-plus"></i> add before</li>
+        <li @click="moveRight()"><i class="fa fa-arrow-right"></i> move right</li>
+        <li @click="moveLeft()"><i class="fa fa-arrow-left"></i> move left</li>
       </ul>
     </div>
   </div>
@@ -44,13 +48,12 @@ export default {
     }
   },
   methods: {
-    showContextMenu(event, id) {
+    showContextMenu(event, instrument, i) {
       event.preventDefault()
-      console.log(event)
-      let thElement = document.getElementById(id)
+      let thElement = document.getElementById(this.instrumentThId(instrument, i))
       let offsetX = thElement.offsetLeft + event.offsetX + 15
       let offsetY = thElement.offsetTop + event.offsetY + 15
-      this.currentContextMenu = {offsetX, offsetY }
+      this.currentContextMenu = { offsetX, offsetY, instrument }
       this.$store.commit("setContextMenuShown", !this.contextMenuShown)
     },
     getContextMenuStyle() {
@@ -61,6 +64,11 @@ export default {
     },
     instrumentThId(instrument, i) {
       return `instrument-th-${instrument.name}-${i}`
+    },
+    deleteInstrument() {
+      this.pattern.instruments = this.pattern.instruments.filter(ins => ins !== this.currentContextMenu.instrument)
+      this.$store.commit("setPattern", this.pattern)
+      localStorage.setItem('pattern', this.pattern.instruments)
     }
   },
   computed: {
