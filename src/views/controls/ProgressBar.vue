1bc109f90435c0b63874308826747d9eb8e9589c
Vadim Semenov <bagum.cemehob@gmail.com>
2024-07-20 21:53:44 +0300

    adjust progress bar styles

--- src/views/controls/ProgressBar.vue
+++ src/views/controls/ProgressBar.vue
@@ -2,7 +2,7 @@
   <div class="bar">
     <div class="current-measure" v-html="getMeasure()"></div>
     <div class="current-time">{{ moment.currentTime.timeString }}</div>
-    <progress id="time" max="100" :value="currentTimePercentage()" @click="startFromTime" />
+    <progress id="time" max="100" :value="currentTimePercentage()" @click="startFromTime"/>
   </div>
 </template>
 
@@ -13,7 +13,7 @@ import {ConductorService} from "@/services/ConductorService";
 
 const store = useStore()
 const pattern = computed(() => store.state.pattern)
-const moment = computed( () =>  {
+const moment = computed(() => {
   return {
     currentBeat: props.beatEmitter.currentBeat,
     currentTime: ConductorService.duration(props.beatEmitter.currentSecond),
@@ -39,7 +39,7 @@ const startFromTime = (event) => {
 const getCurrentBeat = (offsetX, elementWidth) => {
   const beatsTotal = ConductorService.durationInBeats(pattern.value)
   const beatsForPixel = beatsTotal / elementWidth
-  return  Math.floor(offsetX * beatsForPixel)
+  return Math.floor(offsetX * beatsForPixel)
 }
 
 const getMeasure = () => {
@@ -53,7 +53,7 @@ const getMeasure = () => {
 </script>
 
 <style scoped>
-.bar > div{
+.bar > div {
   display: inline-block;
 }
 
@@ -61,11 +61,25 @@ progress {
   margin: 0 0 0 20px;
   cursor: pointer;
   width: 80%;
+  background-color: #000;
+  height: 7px;
 }
+
+progress::-webkit-progress-bar {
+  background-color: rgb(162, 250, 211);
+  border-radius: 7px;
+}
+
+progress::-webkit-progress-value {
+  background-color: #36866e;
+  border-radius: 7px;
+}
+
 .bar {
   display: inline-block;
   width: 90%;
 }
+
 .current-measure {
   color: #58dab4;
   margin: 0 20px;
