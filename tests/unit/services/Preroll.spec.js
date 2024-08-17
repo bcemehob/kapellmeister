8aef0d411a09ed316c751e74bdc17d321915ce56
Vadim Semenov <bagum.cemehob@gmail.com>
2024-08-17 20:03:05 +0300

    add test for Preroll starts and assert Preroll.start() returns a Promise object and test

--- tests/unit/services/Preroll.spec.js
+++ tests/unit/services/Preroll.spec.js
@@ -17,9 +17,24 @@ describe('Preroll', () => {
         expect(preroll.intervalBetweenBeats).toEqual(60 * 1000 / 120) // 500
         expect(preroll.playing).toBeFalsy()
     })
+    it('will start Preroll with not 0 duration and return a Promise', () => {
+        const preroll = new Preroll(120, 1, onPrerollStop)
+        expect(preroll.start()).toStrictEqual(new Promise(_ => {}))
+        let firstBeatTime = preroll.firstBeatTime
+        expect(firstBeatTime).toBeLessThanOrEqual(new Date().getTime())
+        expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), expect.any(Number));
+        expect(setTimeout.mock.calls).toHaveLength(1);
+        let receivedInterval = setTimeout.mock.calls[0][1]
+        expect(receivedInterval / 10).toBeCloseTo(48, 0)
+        expect(preroll.currentBeat).toBe(1)
+        expect(preroll.timeoutId).not.toBeNull()
+        expect(preroll.firstBeatTime).toBe(firstBeatTime)
+        expect(preroll.playing).toBeTruthy()
+        expect(clearTimeout).not.toHaveBeenCalled()
+    })
     it('will stop Preroll after start if current beat value exceeds duration', () => {
         const preroll = new Preroll(120, 0, onPrerollStop)
-        expect(preroll.start()).toStrictEqual(new Promise(resolve => {}))
+        expect(preroll.start()).toStrictEqual(new Promise(_ => {}))
         let firstBeatTime = preroll.firstBeatTime
         expect(firstBeatTime).toBeLessThanOrEqual(new Date().getTime())
         expect(setTimeout).not.toHaveBeenCalled()
