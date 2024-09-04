cc683b5de7ae66f169bdc532c125bdf47f4e1ff0
Vadim Semenov <bagum.cemehob@gmail.com>
2024-09-04 08:09:08 +0300

    Use VueJs Composition API in the root application component

--- src/App.vue
+++ src/App.vue
@@ -3,86 +3,59 @@
   <pattern-editor v-if="editMode" :current-beat="currentBeat"/>
   <conductor v-else :current-beat="currentBeat" :current-preroll-beat="currentPrerollBeat"/>
 </template>
-<script>
-import {ConductorService} from "@/services/ConductorService";
-import Conductor from "@/views/Conductor.vue";
-import PatternEditor from "@/views/editor/PatternEditor.vue";
-import {BeatEmitterProvider} from "@/services/BeatEmitterProvider";
-import TopBar from "@/components/TopBar.vue";
+<script setup>
+import {useStore} from "vuex"
+import {computed, onBeforeUnmount, onMounted, ref, watch} from "vue"
+import {ConductorService} from "@/services/ConductorService"
+import {BeatEmitterProvider} from "@/services/BeatEmitterProvider"
+import TopBar from "@/components/TopBar.vue"
+import PatternEditor from "@/views/editor/PatternEditor.vue"
+import Conductor from "@/views/Conductor.vue"
 
-export default {
-  name: 'App',
-  components: {PatternEditor, Conductor, TopBar},
-  data() {
-    return {
-      beatEmitter: null,
-      serverBeatEmitterEnabled: true,
-      playing: false,
-      wsMessages: [],
-      wsMessage: ''
-    }
-  },
-  methods: {
-    loadPattern() {
-      if (ConductorService.isEmpty(this.pattern)) {
-        const patternJson = localStorage.getItem('pattern')
-        if (patternJson) {
-          this.$store.commit('setPattern', JSON.parse(patternJson))
-        }
-      }
-    },
-    handleClick() {
-      document.addEventListener('click', this.closeContextMenu)
-    },
-    closeContextMenu() {
-      this.$store.commit('setContextMenuShown', false)
-    },
-  },
-  computed: {
-    pattern() {
-      return this.$store.state.pattern
-    },
-    editMode() {
-      return this.$store.state.editMode
-    },
-    currentBeat() {
-      return this.beatEmitter ? this.beatEmitter.currentBeat : 0
-    },
-    currentPrerollBeat() {
-      return this.beatEmitter ? this.beatEmitter.getCurrentPrerollBeat() : 0
-    },
-    prerollBeats() {
-      return ConductorService.isEmpty(this.pattern) ? 0 : this.$store.state.prerollMeasures * this.pattern.measure.beats
-    }
-  },
-  mounted() {
-    this.loadPattern()
-    this.handleClick()
-    if (this.pattern && !ConductorService.isEmpty(this.pattern)) {
-      let duration = ConductorService.durationInBeats(this.pattern);
-      this.beatEmitter = new BeatEmitterProvider(this.pattern.tempo, duration, this.prerollBeats, this.serverBeatEmitterEnabled)
-          .get()
-    }
-  },
-  beforeUnmount() {
-    document.removeEventListener('click', this.closeContextMenu)
-  },
+const beatEmitter = ref(null)
+const serverBeatEmitterEnabled = true
+// const playing = ref(false)
+// const wsMessages = ref([])
+// const wsMessage = ref('')
 
-  watch: {
-    pattern(newVal) {
-      let duration = ConductorService.durationInBeats(this.pattern);
-      this.beatEmitter = ConductorService.isEmpty(newVal) ? null :
-          new BeatEmitterProvider(this.pattern.tempo, duration, this.prerollBeats, this.serverBeatEmitterEnabled).get()
-    },
-    prerollBeats(newVal) {
-      this.beatEmitter && this.beatEmitter.resetPreroll(newVal)
-    },
-    beatEmitter: {
-      handler(newVal) {
-        console.log("BE updated", newVal)
-      },
-      deep: true
+const store = useStore()
+const pattern = computed(() => store.state.pattern)
+const editMode = computed(() => store.state.editMode)
+const currentBeat = computed(() => beatEmitter.value ? beatEmitter.value.currentBeat : 0)
+const currentPrerollBeat = computed(() => beatEmitter.value ? beatEmitter.value.getCurrentPrerollBeat() : 0)
+const prerollBeats = ConductorService.isEmpty(pattern.value) ? 0 : store.state.prerollMeasures * pattern.value.measure.beats
+
+const loadPattern = () => {
+  if (ConductorService.isEmpty(pattern.value)) {
+    const patternJson = localStorage.getItem('pattern')
+    if (patternJson) {
+      console.log("pattern found", patternJson)
+      store.commit('setPattern', JSON.parse(patternJson))
     }
   }
 }
+const handleClick = ()  => document.addEventListener('click', closeContextMenu)
+const closeContextMenu = () => store.commit('setContextMenuShown', false)
+
+onMounted(() => {
+  loadPattern()
+  handleClick()
+  if (pattern.value && !ConductorService.isEmpty(pattern.value)) {
+    console.log("BE ON MOUNT")
+    let duration = ConductorService.durationInBeats(pattern.value)
+    beatEmitter.value = new BeatEmitterProvider(pattern.value.tempo, duration, prerollBeats, serverBeatEmitterEnabled).get()
+  }
+})
+
+onBeforeUnmount(() =>  document.removeEventListener('click', closeContextMenu))
+
+watch(pattern, newVal => {
+  let duration = ConductorService.durationInBeats(newVal)
+  beatEmitter.value = ConductorService.isEmpty(newVal) ? null :
+      new BeatEmitterProvider(newVal.tempo, duration, prerollBeats, serverBeatEmitterEnabled).get()
+})
+watch(prerollBeats, newVal => beatEmitter.value && beatEmitter.value.resetPreroll(newVal))
+watch(beatEmitter, newVal =>  console.log("BE updated", newVal))
+
 </script>
+
