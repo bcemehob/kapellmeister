808317026a9c7e8d81575f28b9ff07ab85efbbee
Vadim Semenov <bagum.cemehob@gmail.com>
2024-10-19 19:29:43 +0300

    show info about available hosts

--- src-backend/NetworkUtil.js
+++ src-backend/NetworkUtil.js
@@ -1,15 +1,16 @@
 const {networkInterfaces} = require('os')
 
-module.exports = function () {
-    const foundInterface = findInterfaceWithIpV4LocalNetworkAddress()
-    if (!foundInterface) {
+module.exports = () => {
+    const foundInterfaces = findInterfaceWithIpV4LocalNetworkAddress()
+    if (!foundInterfaces.length) {
         throw new Error("Not found local ip for incoming connections. Check your network preferences")
     }
-    return foundInterface.address
+    console.log("available hosts: ", foundInterfaces.map(fi => fi.address))
+    return foundInterfaces[0].address
 }
 
 function findInterfaceWithIpV4LocalNetworkAddress() {
     return Object.values(networkInterfaces())
         .flat()
-        .find(netInterface => netInterface.family === 'IPv4' && !netInterface.internal)
+        .filter(netInterface => netInterface.famidly === 'IPv4' && !netInterface.internal)
 }
