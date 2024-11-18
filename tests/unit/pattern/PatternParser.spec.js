fdbe376caeceb7e4264a61efc6b816c4a14242b3
Vadim Semenov <bagum.cemehob@gmail.com>
2024-11-18 21:16:15 +0200

    improve testing of Pattern parser

--- tests/unit/pattern/PatternParser.spec.js
+++ tests/unit/pattern/PatternParser.spec.js
@@ -19,8 +19,10 @@ describe('PatternParser', () => {
     })
 })
 
+const expectedPatternFull = new Pattern('test name', 120, 16, new Measure(4, 4), instruments)
+
 const drumParties = [
-    new Party('68cb2f88-3148-4d35-8d17-6e8a7e3ed777', 'solid', 8, 0, 0, []),
+    new Party('68cb2f88-3148-4d35-8d17-6e8a7e3ed777', 'solid', 16, 0, 0, []),
     new Party('f4961912-92ba-4c8a-8d41-9b8ea4df542a', 'snare trello', 16, 2, 2, [])
 ]
 const saxParties = [
@@ -29,7 +31,7 @@ const saxParties = [
     new Party('9af52c3b-fa2f-4b40-a7d9-ddea8c5a84c2', 'fast chaos', 16, 3, 1)
 ]
 
-const saxCooCooPartyElements = [
+const saxPartyElements = [
     new PartyElement('3d0dafe7-0863-4be1-b9e0-f850cfd537a1', saxParties[0].id, 'CHORDS', 1, 4, 'Am', null),
     new PartyElement('043155d4-21b5-4b54-8be5-6fb2c5edcb18', saxParties[0].id, 'CHORDS', 5, 4, 'E7', null),
     new PartyElement('c8a5daea-83ef-4ac6-adcf-221547f8bbe8', saxParties[0].id, 'CHORDS', 9, 4, 'Dm', null),
@@ -38,25 +40,20 @@ const saxCooCooPartyElements = [
 
 const drumPerformances = [
     new PartyPerformance('85dac6d5-39a7-4b6b-a833-e74814f93732',  1, drumParties[0].id),
-    new PartyPerformance('3ef47e4c-b728-4b02-b8b6-bdd369fa9a48', 25, drumParties[0].id),
-    new PartyPerformance('607bd531-6787-4db9-86bc-2befce9bf9bf', 49, drumParties[0].id),
-    new PartyPerformance('c8a95c5d-9419-465a-96b8-3b061e89e5f3', 9, drumParties[1].id),
-    new PartyPerformance('6fbdff67-475a-459f-bbc5-053d537c4cc2', 41, drumParties[1].id),
+    new PartyPerformance('3ef47e4c-b728-4b02-b8b6-bdd369fa9a48', 9, drumParties[0].id),
+    new PartyPerformance('607bd531-6787-4db9-86bc-2befce9bf9bf', 5, drumParties[1].id),
+    new PartyPerformance('c8a95c5d-9419-465a-96b8-3b061e89e5f3', 13, drumParties[1].id),
 ]
 
 const saxPerformances = [
-    new PartyPerformance('27017e6b-f0e6-417f-a5fd-2444b2ecab80',5, saxParties[0].id),
-    new PartyPerformance('22e37f73-67d0-42eb-a23d-210d89c2ac95',33, saxParties[0].id),
-    new PartyPerformance('b3cde4aa-06f6-4bcb-aa2d-064b4273d126',57, saxParties[0].id),
-    new PartyPerformance('a0aa6924-0b01-4ee0-9c4a-e77fc77e7280',9, saxParties[1].id),
-    new PartyPerformance('941a9500-7e81-4024-af05-cef09446b9d0',41, saxParties[1].id),
-    new PartyPerformance('e407e4d4-d2d2-4047-b411-595a8a17e618',25, saxParties[2].id),
-    new PartyPerformance('e0a0f22d-2640-42e5-bb3b-0d1c25e06641',49, saxParties[2].id),
+    new PartyPerformance('27017e6b-f0e6-417f-a5fd-2444b2ecab80',1, saxParties[0].id),
+    new PartyPerformance('22e37f73-67d0-42eb-a23d-210d89c2ac95',9, saxParties[0].id),
+    new PartyPerformance('941a9500-7e81-4024-af05-cef09446b9d0',5, saxParties[1].id),
+    new PartyPerformance('e407e4d4-d2d2-4047-b411-595a8a17e618',13, saxParties[2].id),
 ]
 
 const instruments = [
     new Instrument('4ef4390b-eb61-40d9-9134-06b499f160aa', 'drum', drumPerformances, drumParties, []),
-    new Instrument('9de4792d-6caf-4ad1-b668-b65328e167c4', 'saxo', saxPerformances, saxParties, saxCooCooPartyElements)
+    new Instrument('9de4792d-6caf-4ad1-b668-b65328e167c4', 'saxo', saxPerformances, saxParties, saxPartyElements)
 ]
 
-const expectedPatternFull = new Pattern('test name', 120, 8, new Measure(4, 3), instruments)
