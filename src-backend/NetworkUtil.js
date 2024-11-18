17ec50d0c0b820609d1e7b132f275b837372b6dc
Vadim Semenov <bagum.cemehob@gmail.com>
2024-11-18 22:04:22 +0200

    Network utils - don't rely to 3rd party libraries, try to discover proper local network on your own

--- src-backend/NetworkUtil.js
+++ src-backend/NetworkUtil.js
@@ -1,13 +1,23 @@
-const {get_active_interface} = require('network')
-const util = require('util');
+const {networkInterfaces} = require('os')
 
 module.exports = () => {
-    const getActiveInterface = util.promisify(get_active_interface)
-    return getActiveInterface()
-        .then(networkInterface => {
-            return networkInterface.ip_address
-        })
-        .catch(err => {
-            throw new Error(err)
-        })
+    const foundInterfaces = findInterfaceWithIpV4LocalNetworkAddress()
+    if (!foundInterfaces.length) {
+        throw new Error("Not found local ip for incoming connections. Check your network preferences")
+    }
+    console.log("available hosts: ", foundInterfaces.map(fi => fi.address))
+    return foundInterfaces[0].address
+}
+
+function findInterfaceWithIpV4LocalNetworkAddress() {
+    const candidates = Object.values(networkInterfaces())
+        .flat()
+        .filter(netInterface => netInterface.family === 'IPv4' && !netInterface.internal)
+    let result = candidates.filter(candidate => candidate.address.startsWith('192.168'))
+    if (result.length) return result
+    for (let i = 16; i < 32; i++) {
+        result = candidates.filter(candidate => candidate.address.startsWith(`172.${i}`))
+        if (result.length) return result
+    }
+    return candidates.filter(candidate => candidate.address.startsWith('10.'))
 }
