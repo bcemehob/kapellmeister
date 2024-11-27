c544637de5900c64f28d2326a19560c727ccb96e
Vadim Semenov <bagum.cemehob@gmail.com>
2024-11-27 17:09:49 +0200

    Migrate Instrument class to TS

---
+++ src/pattern/Instrument.ts
@@ -0,0 +1,19 @@
+import {PartPerformance} from "@/pattern/PartPerformance";
+import {Part} from "@/pattern/Part";
+import {PartElement} from "@/pattern/PartElement";
+
+export class Instrument {
+    id: string
+    name: string
+    partPerformances: PartPerformance[]
+    parts: Part[]
+    partElements: PartElement[]
+
+    constructor(id: string, name:string, partyPerformances: PartPerformance[], parts: Part[], partyElements: PartElement[]) {
+        this.id = id
+        this.name = name
+        this.partPerformances = partyPerformances
+        this.parts = parts
+        this.partElements = partyElements
+    }
+}
