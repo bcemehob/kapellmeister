df8a7cecfc108177b941142bb4d42ddb906aff65
Vadim Semenov <bagum.cemehob@gmail.com>
2024-12-01 10:44:41 +0200

    rename part view property

--- src/pattern/PartViewAtBeat.ts
+++ src/pattern/PartViewAtBeat.ts
@@ -2,18 +2,18 @@ import {BeatValues} from "@/pattern/BeatValues";
 import {PartElement} from "@/pattern/deserialized/PartElement";
 
 export class PartViewAtBeat {
-    partyName: string | undefined
+    partName: string | undefined
     beatValues: BeatValues | null
     currentElements: PartElement[]
     nextPartView: PartViewAtBeat | null
 
     constructor(
-        partyName: string | undefined,
+        partName: string | undefined,
         beatValues: BeatValues | null,
         currentElements: PartElement[],
         nextPartView: PartViewAtBeat | null
     ) {
-        this.partyName = partyName
+        this.partName = partName
         this.beatValues = beatValues
         this.currentElements = currentElements
         this.nextPartView = nextPartView
