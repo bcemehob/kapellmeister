97f09393691b15a9193263d6c9d58d6d7770988e
Vadim Semenov <bagum.cemehob@gmail.com>
2024-12-03 09:23:27 +0200

    rename PatternMapper methods

--- src/pattern/PatternMapper.ts
+++ src/pattern/PatternMapper.ts
@@ -12,25 +12,25 @@ export class PatternMapper {
 
     map(rawPattern: any): Pattern {
         const measure: Measure = new Measure(rawPattern.measure.base, rawPattern.measure.beats)
-        const instruments: Instrument[] = this.parseInstruments(rawPattern.instruments)
+        const instruments: Instrument[] = this.mapInstruments(rawPattern.instruments)
         return Pattern.instance(rawPattern, measure, instruments)
     }
 
-    parseInstruments(rawInstruments: any[]): Instrument[] {
+    mapInstruments(rawInstruments: any[]): Instrument[] {
         const instruments: Instrument[] = []
         if (!rawInstruments || !rawInstruments.length) return instruments
-        rawInstruments.forEach(rawInstrument => instruments.push(this.parseInstrument(rawInstrument)))
+        rawInstruments.forEach(rawInstrument => instruments.push(this.mapInstrument(rawInstrument)))
         return instruments
     }
 
-    parseInstrument(rawInstrument: any): Instrument {
-        const partPerformances: PartPerformance[] = this.parsePartyPerformances(rawInstrument.partPerformances)
-        const parts: Part[] = this.parseParts(rawInstrument.parts)
-        const partElements: PartElement[] = this.parsePartyElements(rawInstrument.partElements)
+    mapInstrument(rawInstrument: any): Instrument {
+        const partPerformances: PartPerformance[] = this.mapPartyPerformances(rawInstrument.partPerformances)
+        const parts: Part[] = this.mapParts(rawInstrument.parts)
+        const partElements: PartElement[] = this.mapPartyElements(rawInstrument.partElements)
         return new Instrument(rawInstrument.id, rawInstrument.name, partPerformances, parts, partElements)
     }
 
-    parsePartyPerformances(rawPartyPerformances: any[]): PartPerformance[] {
+    mapPartyPerformances(rawPartyPerformances: any[]): PartPerformance[] {
         const partyPerformances: PartPerformance[] = []
         if (!rawPartyPerformances || !rawPartyPerformances.length) return partyPerformances
         rawPartyPerformances
@@ -38,14 +38,14 @@ export class PatternMapper {
         return partyPerformances
     }
 
-    parseParts(rawParts: any[]): Part[] {
+    mapParts(rawParts: any[]): Part[] {
         const parts: Part[] = []
         if (!rawParts || !rawParts.length) return parts
         rawParts.forEach(party => parts.push(Part.instance(party)))
         return parts
     }
 
-    parsePartyElements(rawPartElements: any[]): PartElement[] {
+    mapPartyElements(rawPartElements: any[]): PartElement[] {
         const partElements: PartElement[] = []
         if (!rawPartElements || !rawPartElements.length) return partElements
         rawPartElements.forEach(raw => partElements.push(PartElement.instance(raw)))
