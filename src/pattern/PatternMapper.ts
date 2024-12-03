d40c87c663d62ac973665f61295a1282dd9e8b52
Vadim Semenov <bagum.cemehob@gmail.com>
2024-12-03 09:27:27 +0200

    do not publish inner PatternMapper methods

--- src/pattern/PatternMapper.ts
+++ src/pattern/PatternMapper.ts
@@ -12,21 +12,21 @@ export class PatternMapper {
         return Pattern.instance(rawPattern, measure, instruments)
     }
 
-    mapInstruments(rawInstruments: any[]): Instrument[] {
+    private mapInstruments(rawInstruments: any[]): Instrument[] {
         const instruments: Instrument[] = []
         if (!rawInstruments || !rawInstruments.length) return instruments
         rawInstruments.forEach(rawInstrument => instruments.push(this.mapInstrument(rawInstrument)))
         return instruments
     }
 
-    mapInstrument(rawInstrument: any): Instrument {
+    private mapInstrument(rawInstrument: any): Instrument {
         const partPerformances: PartPerformance[] = this.mapPartyPerformances(rawInstrument.partPerformances)
         const parts: Part[] = this.mapParts(rawInstrument.parts)
         const partElements: PartElement[] = this.mapPartyElements(rawInstrument.partElements)
         return new Instrument(rawInstrument.id, rawInstrument.name, partPerformances, parts, partElements)
     }
 
-    mapPartyPerformances(rawPartyPerformances: any[]): PartPerformance[] {
+    private mapPartyPerformances(rawPartyPerformances: any[]): PartPerformance[] {
         const partyPerformances: PartPerformance[] = []
         if (!rawPartyPerformances || !rawPartyPerformances.length) return partyPerformances
         rawPartyPerformances
@@ -34,14 +34,14 @@ export class PatternMapper {
         return partyPerformances
     }
 
-    mapParts(rawParts: any[]): Part[] {
+    private mapParts(rawParts: any[]): Part[] {
         const parts: Part[] = []
         if (!rawParts || !rawParts.length) return parts
         rawParts.forEach(party => parts.push(Part.instance(party)))
         return parts
     }
 
-    mapPartyElements(rawPartElements: any[]): PartElement[] {
+    private mapPartyElements(rawPartElements: any[]): PartElement[] {
         const partElements: PartElement[] = []
         if (!rawPartElements || !rawPartElements.length) return partElements
         rawPartElements.forEach(raw => partElements.push(PartElement.instance(raw)))
